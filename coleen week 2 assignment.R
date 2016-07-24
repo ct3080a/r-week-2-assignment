b266d125264c2d476a31327bb9d035b6bbf8d898
ct3080a <coleen.thomas@spsmail.cuny.edu>
2016-07-24 15:35:36 -0400

    week 2
    my variation of choose function. tried code to display a vector using
    apply

---
+++ coleen week 2 assignment.R
@@ -0,0 +1,13 @@
+##Week 2 Assignment Write your own variation of the choose () function
+install.packages("rmarkdown")
+require(rmarkdown)
+colchoose <- function(n, r)
+  
+({
+    apply(r), function(r), gamma(r)
+})
+
+ r <- -2:5
+ 
+ 
+##code modeled after example found https://stat.ethz.ch/R-manual/R-devel/library/base/html/Special.html
